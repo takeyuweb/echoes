06cf3dc049abdbec17f7f71422f661f20f8b7dd9
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2017-11-22 04:31:09 +0900

    wip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,21 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    prepare_interfaces
+    @interfaces =
+      Socket.getifaddrs.select{|ifaddr| ifaddr.addr.ipv4? || ifaddr.addr.ipv6?}.map { |ifaddr| ifaddr.addr.ip_address }
     @devices = Device.all
   end
 
   def search
     SearchJob.perform_later(params[:interface].presence || '0.0.0.0')
-
-    prepare_interfaces
-    @devices = Device.all
-    render action: :index
-  end
-
-  private
-
-  def prepare_interfaces
-    @interfaces =
-      Socket.getifaddrs.select{|ifaddr| ifaddr.addr.ipv4? || ifaddr.addr.ipv6?}.map { |ifaddr| ifaddr.addr.ip_address }
+    redirect_to root_url, notice: 'スキャンを開始しました。'
   end
 end
